1916edc4c6b70ce182a4a132a1f9cb9e35c87d3e
Robert Buchberger <rbuchberger@gmail.com>
2017-12-12 22:35:54 +0000

    Styles for flash messages and navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  
+  # Hash of bootstrap message types vs rails message types. 
+  BOOTSTRAP_FLASH_MSG = {
+    success: 'alert-success',
+    error: 'alert-error',
+    alert: 'alert-block',
+    notice: 'alert-info'
+  }
+  
+  # Converts rails flash messages to names that bootstrap expects, for proper styling
+  def bootstrap_class_for(flash_type)
+    BOOTSTRAP_FLASH_MSG.fetch(flash_type.to_sym, flash_type)
+  end
+  
+  # Helper test for items which should only display to trainers and admins
+  def is_trainer_or_admin?
+    user_signed_in? && (current_user.trainer? || current_user.admin?)
+  end
+
 end
