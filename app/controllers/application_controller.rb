39d87c1c51e0a753196f570e4575a1b7ab016ed9
Robert Buchberger <rbuchberger@gmail.com>
2017-12-14 20:34:52 +0000

    Refined layouts and user management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up,  keys: [:first_name, :last_name, :badge_number])
-    devise_parameter_sanitizer.permit(:edit, keys: [:first_name, :last_name, :badge_number])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :badge_number])
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+john = User.create(
+  first_name: 'John',
+  last_name: "O'Hagen",
+  badge_number: 'a01',
+  email: 'admin@example.com',
+  role: :admin,
+  password: '123456',
+  password_confirmation: '123456'
+  )
+john.save!
+
+thorny = User.create(
+  first_name: 'Thorny',
+  last_name: 'Ramathorn',
+  badge_number: 'x01',
+  email: 'trainer@example.com',
+  role: :trainer,
+  password: '123456',
+  password_confirmation: '123456'
+  )
+thorny.save!
+
+rabbit = User.create(
+  first_name: 'Rabbit',
+  last_name: 'Roto',
+  badge_number: 'x02',
+  email: 'deputy@example.com',
+  role: :deputy,
+  password: '123456',
+  password_confirmation: '123456'
+  )
+rabbit.save!
+
+farva = User.create(
+  first_name: 'Rod',
+  last_name: 'Farva',
+  badge_number: 'x03',
+  email: 'pending@example.com',
+  role: :pending,
+  password: '123456',
+  password_confirmation: '123456'
+  )
+farva.save!
