7bc32af317d63b950daf6a116f2bdf275ec85ed9
Robert Buchberger <rbuchberger@gmail.com>
2017-12-17 19:27:43 +0000

    Timecard create form works now

---
+++ app/controllers/timecards_controller.rb
@@ -0,0 +1,61 @@
+class TimecardsController < ApplicationController
+  before_action :get_user
+  before_action :get_timecard, only: [:show, :edit, :destroy]
+  before_action :can_edit?, except: [:index, :show]
+
+  def index
+    @timecards = @user.timecards.all
+  end
+  
+  def new
+    @timecard = Timecard.new
+  end
+  
+  def create
+    @timecard = @user.timecards.create(timecard_params)
+    redirect_to timecards_path
+  end
+  
+  def show
+    
+  end
+  
+  def edit
+    
+  end
+  
+  def update
+    @timecard.update!(timecard_params)
+  end
+  
+  def destroy
+    @timecard.destroy!
+    redirect_to timecards_path
+  end
+  
+  private
+  
+  def get_user
+    # If a user is specified in parameters, check to see if  the current user is 
+    # an admin or a trainer. If not specified, set @user to current_user. 
+    if params[:user_id]
+      redirect_to root_path unless (current_user.admin? || current_user.trainer?)
+      @user = User.find(params[:user_id])
+    else
+      @user = current_user
+    end
+  end
+  
+  def can_edit?
+    redirect_to root_path unless @user == current_user
+  end
+
+  def get_timecard
+    @timecard = @user.timecards.find(params[:id])
+  end
+
+  def timecard_params
+    params.require(:timecard).permit(:start, :end, :description)
+  end
+  
+end
--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -1,3 +1,8 @@
 class Timecard < ApplicationRecord
   belongs_to :user
+  
+  # Calculates length of workday, in hours rounded to the nearest decimal place. 
+  def length
+   ((self.end - self.start) / 1.hour ).round(1)
+  end
 end
