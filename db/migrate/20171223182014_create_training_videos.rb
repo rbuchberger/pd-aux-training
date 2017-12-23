ae71e0e7d35fb0ab33cc13213a6f287e5ce75161
Robert Buchberger <rbuchberger@gmail.com>
2017-12-23 22:57:07 +0000

    Before db:migrate

---
+++ db/migrate/20171223182014_create_training_videos.rb
@@ -0,0 +1,13 @@
+class CreateTrainingVideos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :training_videos do |t|
+      t.text :title 
+      t.text :description 
+      t.text :url # URL to video
+      t.text :yt_id # Youtube video id
+      t.integer :custom_start # Custom video start time, seconds
+      t.integer :custom_end # Custom video end time, seconds
+      t.timestamps
+    end
+  end
+end
