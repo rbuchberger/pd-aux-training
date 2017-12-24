8b82fe239d134a178079e7a0fa734f44bd3dee40
Robert Buchberger <rbuchberger@gmail.com>
2017-12-24 20:16:54 +0000

    Controller actions complete

--- app/controllers/training_videos_controller.rb
+++ app/controllers/training_videos_controller.rb
@@ -17,15 +17,15 @@ class TrainingVideosController < ApplicationController
   end
   
   def show 
-    @video = Video.find(:id)
+    @video = get_video
   end
   
   def edit
-    @video = Video.find(:id)
+    @video = get_video
   end
   
   def update
-    @video = Video.find(:id)
+    @video = get_video
     if @video.update(training_video_params)
       flash[:success] = "Training video updated!"
       redirect_to training_video_path(@video)
@@ -40,7 +40,7 @@ class TrainingVideosController < ApplicationController
   end
 
   def delete
-    @video = Video.find(:id)
+    @video = get_video
     if @video.delete
       flash[:success] = "Video deleted!"
       redirect_to training_videos_path
@@ -52,12 +52,15 @@ class TrainingVideosController < ApplicationController
   
   private
   
+  def get_video
+    TrainingVideo.find(:id)
+  
   def trainer_check
     redirect_to root_path unless current_user.trainer?
   end
   
   def training_video_params
-    params.require(:training_video).permit(:title, :description, :custom_start, :custom_end)
+    params.require(:training_video).permit(:title, :description, :custom_start, :custom_end, :url)
   end
   
 end
