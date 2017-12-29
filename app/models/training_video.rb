3799f83080ed77f0355c9468c21fc0ce8fc2b74c
Robert Buchberger <rbuchberger@gmail.com>
2017-12-29 13:43:49 +0000

    new and edit forms should be working now

--- app/models/training_video.rb
+++ app/models/training_video.rb
@@ -3,14 +3,17 @@ class TrainingVideo < ApplicationRecord
   # Validations
   validates :title, presence: true, length: {maximum: 30}
   validates :description, presence: true
+  validates :url, presence: true
   
   # Callbacks
-#  before_save :grab_youtube_id
+  before_save :grab_youtube_id
   
   private
   
   def grab_youtube_id
-    # Sets self.yt_id to the result of some regular expression
+    # Pulls the youtube video ID out of the url. TODO - update this with something less picky
+    yt_regexp = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
+    self.yt_id = yt_regexp.match(self.url)[5]
   end
   
   
