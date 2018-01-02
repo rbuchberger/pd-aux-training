07607eb507a1bf488dc3dd7919df02d453f539a5
Robert Buchberger <rbuchberger@gmail.com>
2018-01-02 03:55:49 +0000

    Training video server side validations

--- app/models/training_video.rb
+++ app/models/training_video.rb
@@ -1,9 +1,13 @@
 class TrainingVideo < ApplicationRecord
-  
+  # Class Variables
+  @@url_regex = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
+
   # Validations
-  validates :title, presence: true, length: {maximum: 30}
-  validates :description, presence: true
+  validates :title, presence: true, length: {maximum: 50}
   validates :url, presence: true
+  validates :description, length: {maximum: 1000}
+  validates_format_of :url, with: @@url_regex, 
+    message: ": I'm sorry, I don't know what to do with that address."
   
   # Callbacks
   before_save :grab_youtube_id
@@ -11,10 +15,8 @@ class TrainingVideo < ApplicationRecord
   private
   
   def grab_youtube_id
-    # Pulls the youtube video ID out of the url. TODO - update this with something less picky
-    yt_regexp = /(youtu\.be\/|youtube\.com\/(watch\?(.*&)?v=|(embed|v)\/))([^\?&"'>]+)/
-    self.yt_id = yt_regexp.match(self.url)[5]
+    # Pulls the youtube video ID out of the url.
+    self.yt_id = @@url_regex.match(self.url)[5] 
   end
-  
   
 end
