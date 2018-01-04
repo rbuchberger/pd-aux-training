b471497537a81834d1a557cf1b01593b1443cdcd
Robert Buchberger <rbuchberger@gmail.com>
2018-01-04 16:31:01 +0000

    Pages controller tests

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class PagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+  
+  # --- Logged out user tests 
+  
+  test "home" do
+    get root_path
+    assert_response :success
+  end
+  
+  # --- Trainer & Admin tests
+  
+  test "home-trainer" do
+    sign_in users(:trainer)
+    get root_path
+    
+    assert_response :success
+  end
+  
+  # --- Deputy tests
+  test "home-deputy" do
+    sign_in users(:deputy)
+    get root_path
+    
+    assert_response :success
+  end
+    
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,49 +6,55 @@ class UserTest < ActiveSupport::TestCase
   
   # Should have a first name
   test "has first name" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.first_name = ""
     
     assert_not t.save
   end
+  
   # First name no longer than 50 chars
   test "long first name" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.first_name = "a" * 51
     
     assert_not t.save
   end
+  
   # Should have a last name
   test "has last name" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.last_name = ""
     
     assert_not t.save
   end  
+  
   # Last name no longer than 50 chars
   test "long last name" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.last_name = "a" * 51
     
     assert_not t.save
-  end  
+  end 
+  
   # Should have a badge number
   test "has badge number" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.badge_number = ""
     
     assert_not t.save
   end  
+  
   # Badge number no longer than 4 chars
   test "long badge number" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.badge_number = "12345"
     
     assert_not t.save
-  end  
+  end 
+  
   # Should capitalize string fields before save
   test "format strings" do
-    t = User.new(valid_test_pending)
+    t = User.new(valid_test_params)
     t.first_name = "asdf"
     t.last_name = "ASDF"
     t.badge_number = "abC2"
@@ -59,76 +65,47 @@ class UserTest < ActiveSupport::TestCase
     assert t.last_name = "Asdf"
     assert t.badge_number = "ABC2"
   end
+  
   # Newly created users should be set to pending
   test "pending new" do
-    t = User.new(valid_test_admin)
+    t = User.new(valid_test_params)
+    t.role = :admin
     t.save
     
     assert t.role = :pending
   end
+  
   # pending users should not be considered active for authentication
   test "pending active" do
-    t = User.create(valid_test_pending)
+    t = User.create(valid_test_params)
     
     assert_not t.active_for_authentication?
   end
+  
   # first_last(length) custom method
   test "first last" do
     t = users(:admin)
     
     assert t.first_last == "Austin Powers"
   end
+  
   # last_first(length) custom method
   test "last, first" do
     t = users(:admin)
     
     assert t.last_first == "Powers, Austin"
   end
+  
   # admins should be trainers too
   test "admins are trainers" do
     t = users(:admin)
     
     assert t.trainer?
   end
+  
   private
   
-  def valid_test_trainer
-    {
-    first_name: 'Vanessa',
-    last_name: 'Kensington',
-    badge_number: 'x01',
-    email: 'trainer@example.com',
-    role: :trainer,
-    password: '123456',
-    password_confirmation: '123456'
-  }
-  end
-  
-  def valid_test_admin
-    {
-    first_name: 'Austin',
-    last_name: 'Powers',
-    badge_number: 'a01',
-    email: 'admin@example.com',
-    role: :admin,
-    password: '123456',
-    password_confirmation: '123456'
-  }
-  end
-  
-  def valid_test_deputy
-    {
-    first_name: 'Scott',
-    last_name: 'Evil',
-    badge_number: 'x02',
-    email: 'deputy@example.com',
-    role: :deputy,
-    password: '123456',
-    password_confirmation: '123456'
-  }
-  end
-  
-  def valid_test_pending
+  def valid_test_params
     {
     first_name: 'Frau',
     last_name: 'Farbissina',
@@ -137,7 +114,7 @@ class UserTest < ActiveSupport::TestCase
     role: :pending,
     password: '123456',
     password_confirmation: '123456'
-  }
+    }
   end
   
 end
