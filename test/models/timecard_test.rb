55343f8009ea1694899936348bf6b3e7fa488a1e
Robert Buchberger <rbuchberger@gmail.com>
2018-01-04 03:34:31 +0000

    added test fixtures

--- test/models/timecard_test.rb
+++ test/models/timecard_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class TimecardTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Timecard should not save without a description
+  
+  # should not save without start value
+  
+  # should not save without end value
+  
+  # should not save without a user
+  
+  # should not save if overlaps another timecard
+  
+  # should not save if more than 24 hours
+  
+  # should not save if less than 30 minutes
+  
+  # should return a duration
+  
+  # should return a duration in hours
+  
 end
