9ddde7d5c8e3dcb6ff3338f7edd68867e6910c74
Robert Buchberger <rbuchberger@gmail.com>
2018-01-04 04:10:09 +0000

    timecard model tests

--- app/models/concerns/timecard_validator.rb
+++ app/models/concerns/timecard_validator.rb
@@ -3,40 +3,44 @@ class TimecardValidator < ActiveModel::Validator
   
   def validate(timecard)
     
-    if timecard.start >= Time.zone.now
-       timecard.errors[:start] << "can't be in the future." 
-    end
+    timecard.errors[:start] << "Must be present" unless timecard.start
+    timecard.errors[:end] << "Must be present" unless timecard.end
     
-    if timecard.end >= Time.zone.now + 24.hours
-      timecard.errors[:end] << "can't be more than 24 hours in the future." 
-    end
-    
-    # Gonna use length several times. 
-    length = timecard.duration
-    if length >= 24.hours
-       timecard.errors[:base] << 
-       "Workday is too long. If you really did work more than 24 hours, log 2 timecards." 
-    elsif length < 0
-      timecard.errors[:base] << 
-      "I think you have your times swapped. End needs to come after start."
-    elsif length >= 0 && length < 30.minutes
-      timecard.errors[:base] << 
-      "Don't log days shorter than 30 minutes."
-    end
-    
-    # Get all the user's timecards 
-    existing_user_timecards = Timecard.where(user_id: timecard.user_id)
-    # Check if they overlap the new card
-    overlaps_existing = false
-    existing_user_timecards.each do |t| 
-      if ( (t[:start]..t[:end]).overlaps? (timecard[:start]..timecard[:end]) ) && t != timecard
-       overlaps_existing = true  
+    unless timecard.errors.any? # All of this breaks if start and end times aren't supplied. 
+      if timecard.start >= Time.zone.now
+         timecard.errors[:start] << "can't be in the future." 
+      end
+      
+      if timecard.end >= Time.zone.now + 24.hours
+        timecard.errors[:end] << "can't be more than 24 hours in the future." 
+      end
+      
+      # Gonna use length several times. 
+      length = timecard.duration
+      if length >= 24.hours
+         timecard.errors[:base] << 
+         "Workday is too long. If you really did work more than 24 hours, log 2 timecards." 
+      elsif length < 0
+        timecard.errors[:base] << 
+        "I think you have your times swapped. End needs to come after start."
+      elsif length >= 0 && length < 30.minutes
+        timecard.errors[:base] << 
+        "Don't log days shorter than 30 minutes."
+      end
+      
+      # Get all the user's timecards 
+      existing_user_timecards = Timecard.where(user_id: timecard.user_id)
+      # Check if they overlap the new card
+      overlaps_existing = false
+      existing_user_timecards.each do |t| 
+        if ( (t[:start]..t[:end]).overlaps? (timecard[:start]..timecard[:end]) ) && t != timecard
+         overlaps_existing = true  
+        end
+      end
+      if overlaps_existing
+        timecard.errors[:base] << "These times overlap with an existing workday."
       end
     end
-    if overlaps_existing
-      timecard.errors[:base] << "These times overlap with an existing workday."
-    end
-    
   end
   
 end
--- test/models/timecard_test.rb
+++ test/models/timecard_test.rb
@@ -2,19 +2,81 @@ require 'test_helper'
 
 class TimecardTest < ActiveSupport::TestCase
   # Timecard should not save without a description
-  
+  test "Timecard save without description" do
+    t = Timecard.new({
+      # description: "I'm here!",
+      start: Time.zone.today - 1.hours,
+      end: Time.zone.today,
+      user: users(:deputy)
+      })
+      assert_not t.save
+  end
   # should not save without start value
-  
+  test "Timecard save without start" do
+    t = Timecard.new({
+      description: "I'm here!",
+      # start: Time.zone.today - 1.hours,
+      end: Time.zone.today,
+      user: users(:deputy)
+      })
+      assert_not t.save
+  end
   # should not save without end value
-  
+    test "Timecard save without end" do
+    t = Timecard.new({
+      description: "I'm here!",
+      start: Time.zone.today - 1.hours,
+      # end: Time.zone.today,
+      user: users(:deputy)
+      })
+      assert_not t.save
+  end
   # should not save without a user
-  
+    test "Timecard save without user" do
+    t = Timecard.new({
+      description: "I'm here!",
+      start: Time.zone.today - 1.hours,
+      end: Time.zone.today
+      # user: users(:deputy)
+      })
+      assert_not t.save
+  end
   # should not save if overlaps another timecard
-  
+    test "Timecard overlap" do
+    Timecard.create({
+      description: "I'm here!",
+      start: Time.zone.today - 3.hours,
+      end: Time.zone.today - 1.hours,
+      user: users(:deputy)
+      })
+    t2 = Timecard.new({
+      description: "I'm here!",
+      start: Time.zone.today - 2.hours,
+      end: Time.zone.today,
+      user: users(:deputy)
+      })
+      assert_not t2.save
+    end
   # should not save if more than 24 hours
-  
+  test "Timecard over 24 hours" do
+    t = Timecard.new({
+      description: "I'm here!",
+      start: Time.zone.today - 25.hours,
+      end: Time.zone.today,
+      user: users(:deputy)
+      })
+      assert_not t.save
+  end
   # should not save if less than 30 minutes
-  
+  test "Timecard less than 30 minutes" do
+    t = Timecard.new({
+      description: "I'm here!",
+      start: Time.zone.today - 15.minutes,
+      end: Time.zone.today,
+      user: users(:deputy)
+      })
+      assert_not t.save
+  end
   # should return a duration
   
   # should return a duration in hours
