99213f858bdc15b69032287a1bcc6ad1ed11dbbe
Robert Buchberger <rbuchberger@gmail.com>
2018-01-05 16:33:16 -0500

    Generated and ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180102192445) do
+ActiveRecord::Schema.define(version: 20180105213200) do
 
   create_table "timecards", force: :cascade do |t|
     t.integer "user_id"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 20180102192445) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.datetime "deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
