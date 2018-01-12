107f2d0f12b771377bbaa280ff5fbdd29da2cbb0
Robert Buchberger <rbuchberger@gmail.com>
2018-01-11 23:48:32 -0500

    WIP moving most of the training_video responsibilities over to training_requirements

--- app/models/training_requirement.rb
+++ app/models/training_requirement.rb
@@ -2,4 +2,5 @@ class TrainingRequirement < ApplicationRecord
   has_many :training_records, dependent: :destroy
   has_many :users, through: :training_records
   has_many :training_videos, dependent: :destroy
+  accepts_nested_attributes_for :training_videos
 end
