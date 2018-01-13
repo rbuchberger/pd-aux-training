129b5a1bd3f91de3603a85ad0103cd5721ceffd6
Robert Buchberger <rbuchberger@gmail.com>
2018-01-13 13:02:00 -0500

    lesson test rewritten

--- test/models/lesson_test.rb
+++ test/models/lesson_test.rb
@@ -1,23 +1,23 @@
 require 'test_helper'
 
-class TrainingRequirementTest < ActiveSupport::TestCase
+class LessonTest < ActiveSupport::TestCase
   # Shouldn't save without a title
   test "no title" do
-    t = TrainingRequirement.new(valid_training_requirement_params)
+    t = Lesson.new(valid_lesson_params)
     t.title = ""
     
     assert_not t.save
   end
   # Title can't be longer than 50 characters
   test "long title" do
-    t = TrainingRequirement.new(valid_training_requirement_params)
+    t = Lesson.new(valid_lesson_params)
     t.title = "c" * 51
     
     assert_not t.save
   end
   # Description can't be longer than 1000 characters
   test "long description" do
-    t = TrainingRequirement.new(valid_training_requirement_params)
+    t = Lesson.new(valid_lesson_params)
     t.description = "I" * 1001
   
     assert_not t.save
