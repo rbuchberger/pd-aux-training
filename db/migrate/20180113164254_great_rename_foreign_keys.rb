72f229e67e600b1ce70a1cbdc8a6ed7a92a67743
Robert Buchberger <rbuchberger@gmail.com>
2018-01-13 12:18:09 -0500

    before migration 2

--- db/migrate/20180113164254_great_rename_foreign_keys.rb
+++ db/migrate/20180113164254_great_rename_foreign_keys.rb
@@ -1,11 +1,11 @@
 class GreatRenameForeignKeys < ActiveRecord::Migration[5.1]
   def change
-    remove_foreign_key :completions, :training_requirement_id
+    remove_foreign_key :completions, :training_requirement
     rename_column :completions, :training_requirement_id, :lesson_id
-    add_foreign_key :completions, :lesson_id, 
+    add_foreign_key :completions, :lesson, 
 
-    remove_foreign_key :videos, :training_requirement_id
+    remove_foreign_key :videos, :training_requirement
     rename_column :videos, :training_requirement_id, :lesson_id
-    add_foreign_key :videos, :lesson_id
+    add_foreign_key :videos, :lesson
   end
 end
