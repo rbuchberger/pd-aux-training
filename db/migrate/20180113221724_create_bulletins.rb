0a24d92336c4f9a54f1bfd65efc193511b060465
Robert Buchberger <rbuchberger@gmail.com>
2018-01-13 17:20:00 -0500

    ran migration

--- db/migrate/20180113221724_create_bulletins.rb
+++ db/migrate/20180113221724_create_bulletins.rb
@@ -1,6 +1,8 @@
 class CreateBulletins < ActiveRecord::Migration[5.1]
   def change
     create_table :bulletins do |t|
+      t.text :title
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180113172828) do
+ActiveRecord::Schema.define(version: 20180113221724) do
+
+  create_table "bulletins", force: :cascade do |t|
+    t.text "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "completions", force: :cascade do |t|
     t.integer "user_id"
