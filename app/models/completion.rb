7cff212e42c5d82f560b9a42f77a393ff5530705
Robert Buchberger <rbuchberger@gmail.com>
2018-01-13 12:34:20 -0500

    completion model renaming

--- app/models/completion.rb
+++ app/models/completion.rb
@@ -1,10 +1,10 @@
-class TrainingRecord < ApplicationRecord
+class Completion < ApplicationRecord
   # Each instance of this class represents the completion of one training video by one user. 
  
-  belongs_to :training_requirement
+  belongs_to :lesson
   belongs_to :user
   
-  validates :training_requirement_id, presence: true
+  validates :lesson_id, presence: true
   validates :user_id, presence: true
   
   # Allows viewing of deactivated users' records. 
