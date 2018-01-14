e689cbcf42728ffe71b2eafe343bbeb72752542d
Robert Buchberger <rbuchberger@gmail.com>
2018-01-13 19:15:13 -0500

    generated controller, started writing actions

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,2 +1,8 @@
 class Bulletin < ApplicationRecord
+  # Validations
+  validates :title, presence: true, length: {maximum: 50}
+  validates :description, length: {maximum: 1000}
+
+  # Scopes
+  default_scope {where(:created_at < Time.zone.today - 30.days )} 
 end
---
+++ app/policies/bulletin_policy.rb
@@ -0,0 +1,19 @@
+class BulletinPolicy < ApplicationPolicy
+
+  def index?
+    true
+  end
+
+  def create?
+    @user.trainer?
+  end
+
+  def update?
+    @user.trainer?
+  end
+
+  def destroy?
+    @user.trainer?
+  end
+
+end
