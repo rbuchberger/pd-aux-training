9510e4c32a85a98113deca6daa31c274b9c28752
Robert Buchberger <rbuchberger@gmail.com>
2018-01-16 00:57:34 -0500

    Building bulletins controller

--- app/controllers/bulletins_controller.rb
+++ app/controllers/bulletins_controller.rb
@@ -1,19 +1,51 @@
 class BulletinsController < ApplicationController
   def new
+    @bulletin = current_user.build_bulletin
   end
 
   def create
+    @bulletin = current_user.build_bulletin(bulletin_params)
+    if @bulletin.save
+      flash [:success] = "Bulletin created!"
+    else
+      flash [:alert] = "Could not save bulletin"
+    end
   end
 
   def edit
+    @bulletin = get_bulletin
   end
 
   def update
+    @bulletin = get_bulletin
+    if @bulletin.update(bulletin_params)
+      flash[:success] = 'Bulletin updated!'
+    else
+      flash[:alert] = 'Could not update bulletin'
+    end
   end
 
   def destroy
+    @bulletin = get_bulletin
+    if @bulletin.destroy
+      flash[:success] = 'Bulletin deleted!'
+    else
+      flash[:alert] = 'Could not delete bulletin'
+    end
   end
 
   def show
+    @bulletin = get_bulletin
   end
+
+  private
+
+  def get_bulletin
+    Bulletin.where(params[:id])
+  end
+
+  def bulletin_params
+    params.require(:bulletin).permit(:title, :body)
+  end
+
 end
--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,4 +1,7 @@
 class Bulletin < ApplicationRecord
+  # Associations
+  belongs_to :user
+
   # Validations
   validates :title, presence: true, length: {maximum: 50}
   validates :description, length: {maximum: 1000}
