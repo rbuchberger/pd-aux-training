3d1945f66ea9664b7f1993bf0b2b0b1d189b08ec
Robert Buchberger <rbuchberger@gmail.com>
2018-02-11 06:26:01 -0500

    switched to using attribute instead of attr_accessor in the model

--- app/models/timecard.rb
+++ app/models/timecard.rb
@@ -5,9 +5,9 @@ class Timecard < ApplicationRecord
   # virtual attributes, and those values get parsed and combined for the actual database entries in a
   # before_validation callback. Clock out date is calculated based on the clock in date and common sense. 
   # Read values for these fields (so the form default values reflect database values) have custom methods.
-  attribute :field_clock_in_date, :datetime, default: Time.zone.now.strftime("%Y-%m-%d") 
-  attribute :field_clock_in_time, :hash   
-  attribute :field_clock_out_time, :hash 
+  attribute :field_clock_in_date, :string, default: Time.zone.now.strftime("%Y-%m-%d") 
+  attribute :field_clock_in_time, :datetime   
+  attribute :field_clock_out_time, :datetime 
   
   # Relationships:
   belongs_to :user
@@ -24,34 +24,32 @@ class Timecard < ApplicationRecord
 
   # This method turns the form field inputs into usable datetimes for the database. 
   def combine_fields
-    if @field_clock_in_date && @field_clock_in_time && @field_clock_out_time
-      self.clock_in = Time.zone.parse(
-        "#{@field_clock_in_date} #{@field_clock_in_time[:hour]}:#{@field_clock_in_time[:minute]}"
-      ) 
-     
-      # Note that clock_out date is initially assumed to be the same as clock_in date.
-      self.clock_out = Time.zone.parse(
-        "#{@field_clock_in_date} #{@field_clock_out_time[:hour]}:#{@field_clock_out_time[:minute]}"
-      ) 
-     
-      # Timecards will never be more than 24 hours long. If clock_out is earlier than clock_in 
-      # we assume the user worked past midnight. 
-      self.clock_out += 1.day if self.clock_in >= self.clock_out
-    end
+    self.clock_in = Time.zone.parse(
+      "#{self.field_clock_in_date} #{self.field_clock_in_time.hour}:#{self.field_clock_in_time.min}"
+    ) 
+   
+    # Note that clock_out date is initially assumed to be the same as clock_in date.
+    self.clock_out = Time.zone.parse(
+      "#{self.field_clock_in_date} #{self.field_clock_out_time.hour}:#{self.field_clock_out_time.min}"
+    ) 
+   
+    # Timecards will never be more than 24 hours long. If clock_out is earlier than clock_in 
+    # we assume the user worked past midnight. 
+    self.clock_out += 1.day if self.clock_in >= self.clock_out
   end
 
   # Custom attributes:
 
   def field_clock_in_date
-    self.clock_in ? self.clock_in.strftime("%Y-%m-%d") : Time.zone.now.strftime("%Y-%m-%d")
+    self.clock_in ? self.clock_in.strftime("%Y-%m-%d") : super
   end
 
   def field_clock_in_time
-    self.clock_in ? self.clock_in : Time.zone.now
+    self.clock_in ? self.clock_in : super
   end
 
   def field_clock_out_time
-    self.clock_out ? self.clock_out : Time.zone.now
+    self.clock_out ? self.clock_out : super
   end
 
   # Length of workday 
