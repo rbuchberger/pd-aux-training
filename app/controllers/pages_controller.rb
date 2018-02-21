afb0989da0ae772dae2337c1e56114ddb0cda439
Robert Buchberger <rbuchberger@gmail.com>
2018-02-21 12:36:37 +0100

    code cleanup, reducing database queries

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,6 @@ class PagesController < ApplicationController
   def home
     @user = current_user
     if @user
-      @incomplete_training_count = Lesson.count - @user.completions.count
       @last_workday = @user.timecards.last
       @pending_count = User.where(role: :pending).count if policy(User).index?
       @bulletins = Bulletin.last(30)
