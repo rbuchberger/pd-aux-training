c0516fe49655ab80f33093a778a0873cae940c57
Robert Buchberger <rbuchberger@gmail.com>
2018-03-21 16:53:13 +0100

    excised coffeescript. Tests passing

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,16 @@ Bundler.require(*Rails.groups)
 module PdAuxTraining
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-    config.time_zone = 'Eastern Time (US & Canada)'
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.load_defaults 5.1
+    config.time_zone = 'Eastern Time (US & Canada)'
+
+    config.generators do |g|
+      g.javascript_engine = :js
+    end
+
   end
 end
