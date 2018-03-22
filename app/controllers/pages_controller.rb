5d498daca2cd0f5a4719cd4499ab2b5406ebe23d
Robert Buchberger <rbuchberger@gmail.com>
2018-03-22 11:05:48 +0100

    refactored logged in homepage content into helper and partials

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
-    if @current_user 
-      @last_workday = @current_user.timecards.last
+    if user_signed_in?
+      @last_workday = current_user.timecards.last
       @pending_count = User.where(role: :pending).count if policy(User).index?
       @bulletins = Bulletin.all_cached
     end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,33 @@
 module PagesHelper
+
+  def dynamic_homepage_content
+    if user_signed_in?
+      render 'logged_in'
+    else
+      render 'logged_out'
+    end
+  end
+
+  def last_workday
+    render 'last_workday'
+  end
+
+  def pending_users
+    if policy(User).index? && @pending_count > 0
+      render 'pending_users'
+    end
+  end
+
+  def bulletins
+    render partial: 'bulletins/bulletin', collection: @bulletins, cached: true
+  end
+
+  def create_bulletin
+    render 'create_bulletin' if policy(Bulletin).create?
+  end
+
+  def under_construction
+    render 'under_construction'
+  end
+
 end
