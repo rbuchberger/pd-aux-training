097cc712db202b7f0cf50118a2663df277316f90
Robert Buchberger <rbuchberger@gmail.com>
2018-03-23 13:16:14 +0100

    added bulletin model tests

--- test/models/bulletin_test.rb
+++ test/models/bulletin_test.rb
@@ -4,4 +4,19 @@ class BulletinTest < ActiveSupport::TestCase
   # The bulletin model is pretty simple. 
   # I'm not writing tests for basic rails functionality such as validations. 
 
+  test "Cache fetch" do
+    t = Bulletin.all_cached.count
+    # Test case only has 3 bulletins
+    last_30 = Bulletin.last(30).count
+
+    assert_equal t, last_30
+  end
+
+  test "clear_cache" do
+    t = Bulletin.all_cached.count
+    Bulletin.create(valid_bulletin_params)
+
+    assert_not_equal t, Bulletin.all_cached.count
+  end
+
 end
