edc2e44b0aec7c908c989112783d01279219600d
Robert Buchberger <rbuchberger@gmail.com>
2018-04-07 14:16:36 +0200

    controller test skeleton

--- test/controllers/documents_controller_test.rb
+++ test/controllers/documents_controller_test.rb
@@ -1,7 +1,80 @@
 require 'test_helper'
 
-class DocumentsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+# Deputy tests
+class DocumentsControllerDeputyTest < ActionDispatch::IntegrationTest
+
+  setup do
+    stub_aws
+    sign_in_users[:deputy]
+  end
+
+  teardown do
+    unstub_aws
+  end
+
+  # index
+
+  # show
+
+  # new
+
+  # create
+
+  # edit
+
+  # update
+
+  # destroy
+
+end
+
+# Trainer tests
+class DocumentsControllerTrainerTest < ActionDispatch::IntegrationTest
+
+  setup do
+    stub_aws
+    sign_in_users[:trainer]
+  end
+
+  teardown do
+    unstub_aws
+  end
+
+  # new
+
+  # create
+
+  # edit
+
+  # update
+
+  # destroy
+
+end
+
+# Not logged in tests
+class DocumentsControllerGuestTest < ActionDispatch::IntegrationTest
+
+  setup do
+    stub_aws
+  end
+
+  teardown do
+    unstub_aws
+  end
+
+  # index
+
+  # show
+
+  # new
+
+  # create
+
+  # edit
+
+  # update
+
+  # destroy
+
 end
