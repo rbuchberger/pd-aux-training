0b1d113343b52af776a4f3c09abafd6d928b0104
Robert Buchberger <rbuchberger@gmail.com>
2018-04-17 17:55:57 +0200

    set default values

--- db/migrate/20180417153137_create_preferences.rb
+++ db/migrate/20180417153137_create_preferences.rb
@@ -4,12 +4,12 @@ class CreatePreferences < ActiveRecord::Migration[5.1]
       t.belongs_to :user, index: true
 
       # All users:
-      t.boolean :bulletin_notify
-      t.boolean :document_notify
+      t.boolean :bulletin_notify, default: true
+      t.boolean :document_notify, default: false
       
       # Admin related:
-      t.boolean :user_signup_notify
-      t.boolean :user_deactivate_notify
+      t.boolean :user_signup_notify, default: false
+      t.boolean :user_deactivate_notify, default: false
       t.timestamps
     end
   end
