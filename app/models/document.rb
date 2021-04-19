17f9abff33d7c4d3b20fe82357a8addb1bf44ce9
Robert Buchberger <robert@buchberger.cc>
2021-04-19 18:30:43 +0200

    Add ActiveStorage to Document

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,46 +1,28 @@
 class Document < ApplicationRecord
-  ALLOWED_FILE_TYPES =
-    [
-      # PDF
-      %r{\Aapplication/pdf},
-
-      # Text Files
-      %r{\Atext/plain},
-
-      # Images
-      %r{\Aimage/jpeg},
-      %r{\Aimage/png},
-
-      # Word Documents
-      %r{\Aapplication/msword},
-      %r{\Aapplication/vnd.openxmlformats-officedocument.wordprocessingml.document},
-
-      # Excel files
-      %r{\Aapplication/vnd.ms-excel},
-      %r{\Aapplication/vnd.openxmlformats-officedocument.spreadsheetml.sheet},
-
-      # Powerpoint presentations
-      %r{\Aapplication/vnd.ms-powerpoint},
-      %r{\Aapplication/vnd.openxmlformats-officedocument.presentationml.presentation}
-    ].freeze
-
-    MAXIMUM_FILE_SIZE = 100.megabytes
-
-  validates :name, length: { maximum: 1000 }
-  validates :description, length: { maximum: 50_000 }
+  MAXIMUM_FILE_SIZE = 100.megabytes
+  has_one_attached :file
 
   before_save :set_name
 
-  def s3_credentials
-    {
-      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      secret_access_key: ENV["AWS_SECRET_KEY"],
-      bucket: ENV["AWS_BUCKET"]
-    }
+  validates :name, length: { maximum: 1000 }
+  validates :description, length: { maximum: 50_000 }
+  validates :file, presence: true
+  validate :acceptable_file_size
+
+  def ext
+    file.filename.extension
   end
+
+  private
 
   # If the custom name field is blank, use the filename.
   def set_name
-    # self.name = file_file_name if name.blank?
+    self.name = file.filename if name.blank?
+  end
+
+  def acceptable_file_size
+    return unless file.attached? && file.byte_size > MAXIMUM_FILE_SIZE
+
+    errors.add(:file, "cannot be larger than 100 megabytes.")
   end
 end
